428a171def74ec04220b46730cb8bebd219680e8
Virginie <vb@ikimo9.com>
2022-12-14 09:34:25 +0100

    style title app

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <h1 class="text-white-800">Planéo</h1>
+                    <h1 class="text-gray-900 dark:text-white">Planéo</h1>
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
